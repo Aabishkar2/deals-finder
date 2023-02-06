c83ffb5b540f49ef9f42798914811557e9918db1
Aabishkar <aabishkar2@gmail.com>
2023-02-06 15:02:11 +0545

    Add page for deals and home

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700|Source+Sans+Pro:400,600,700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
